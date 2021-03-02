343869230fef71c6653248c836d403ad7d7e0400
jaydeep.gohil <jaydeep.gohil@CORP.AMAZONWORKSPACES.COM>
2021-03-02 14:37:35 +0530

    Code42 testing purpose only

---
+++ JavaBasics/src/basicOfJava/Jack-Dummy.java
@@ -0,0 +1,17 @@
+package basicOfJavatest;
+
+public class DataTypesTestHi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+		double d = 1234567890;
+		String str = "My testing";
+		boolean b = true;
+		
+		System.out.println("This is my integer variable-> " + i);
+		System.out.println("This is my double variable-> " + d);
+		System.out.println("This is my string variable-> " + str);
+		System.out.println("This is my boolean variable-> " + b);
+	}
+}
